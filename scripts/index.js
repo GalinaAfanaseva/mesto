15483bac590ae81c72287daa880a689eb668a8aa
Galina Afanaseva <galyafanaseva89@yandex.ru>
2022-01-14 18:12:18 -0500

    feature: add javascript code

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector('.popup');
+//popup.classList.add('popup_opened');
+let changeProfileButton = document.querySelector('.edit-button');
+
+changeProfileButton.addEventListener('click', function() {console.log('Clicked');});
+
+
+
+//console.log(popup.classList);
